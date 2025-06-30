45a8e2cedfcee923923b73656c4bbd5576383420
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-06-29 23:55:27 -0300

    fix(debian-setup): adjust quoting to allow expansion
    - Add 'libxcb-xfixes0-dev' to awesome window manager dependencies

--- functions/awesome-window-manager.sh
+++ functions/awesome-window-manager.sh
@@ -2,7 +2,7 @@ install_awesome_window_manager() {
   echo "[debian-setup] Installing Awesome window manager"
 
   sudo apt update
-  sudo apt install -y gir1.2-rsvg-2.0 git
+  sudo apt install -y gir1.2-rsvg-2.0 git libxcb-xfixes0-dev
   sudo apt build-dep -y awesome
 
   local SOURCE_DIR="$HOME/awesome"
@@ -11,7 +11,7 @@ install_awesome_window_manager() {
 
   make -C "$SOURCE_DIR" package
 
-  sudo apt install -y "$SOURCE_DIR/build/*.deb"
+  sudo apt install -y "$SOURCE_DIR"/build/*.deb
 
   rm -rf "$SOURCE_DIR"
 }
--- functions/kitty-terminal.sh
+++ functions/kitty-terminal.sh
@@ -6,10 +6,10 @@ install_kitty_terminal() {
   local INSTALL_DIR="$HOME/.local/kitty.app"
   local DESKTOP_FILES_DIR="$HOME/.local/share/applications"
 
-  ln -sf "$INSTALL_DIR/bin/{kitty,kitten}" "$HOME/.local/bin/"
+  ln -sf "$INSTALL_DIR"/bin/{kitty,kitten} "$HOME/.local/bin"
 
-  cp "$INSTALL_DIR/share/applications/kitty{,-open}.desktop" "$DESKTOP_FILES_DIR"
+  cp "$INSTALL_DIR"/share/applications/kitty{,-open}.desktop "$DESKTOP_FILES_DIR"
 
-  sed -i "s|Icon=kitty|Icon=$INSTALL_DIR/share/icons/hicolor/256x256/apps/kitty.png|g" "$DESKTOP_FILES_DIR/kitty*.desktop"
-  sed -i "s|Exec=kitty|Exec=$INSTALL_DIR/bin/kitty|g" "$DESKTOP_FILES_DIR/kitty*.desktop"
+  sed -i "s|Icon=kitty|Icon=$INSTALL_DIR/share/icons/hicolor/256x256/apps/kitty.png|g" "$DESKTOP_FILES_DIR"/kitty*.desktop
+  sed -i "s|Exec=kitty|Exec=$INSTALL_DIR/bin/kitty|g" "$DESKTOP_FILES_DIR"/kitty*.desktop
 }
