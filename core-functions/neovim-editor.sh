c957cbf61b92e73cc2bc7e9301066e89cfd27105
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-07-05 17:42:28 -0300

    refactor(neovim-editor): merge variable declarations into a single line
    - Add EXEC_FILES_DIR for user-specific executable files directory
    - Ensure EXEC_FILES_DIR exists before creating a symlink

--- core-functions/neovim-editor.sh
+++ core-functions/neovim-editor.sh
@@ -1,25 +1,26 @@
 install_neovim_editor() {
   display_header "Installing Neovim editor"
 
+  local NEOVIM_TARBALL SHA256_CHECKSUMS NEOVIM_DIR NEOVIM_SHA256_CHECKSUM
+
   local NEOVIM_TARBALL_URL=https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
   local SHA256_CHECKSUMS_URL=https://github.com/neovim/neovim/releases/download/stable/shasum.txt
 
   wget -P "$HOME" "$NEOVIM_TARBALL_URL" "$SHA256_CHECKSUMS_URL"
 
-  local NEOVIM_TARBALL
   NEOVIM_TARBALL="$HOME/$(basename "$NEOVIM_TARBALL_URL")"
-  local SHA256_CHECKSUMS
   SHA256_CHECKSUMS="$HOME/$(basename "$SHA256_CHECKSUMS_URL")"
-  local NEOVIM_DIR
   NEOVIM_DIR="$HOME/$(basename -s ".tar.gz" "$NEOVIM_TARBALL_URL")"
 
-  local NEOVIM_SHA256_CHECKSUM
   NEOVIM_SHA256_CHECKSUM=$(sha256sum "$NEOVIM_TARBALL" | awk '{print $1}')
 
   if grep -q "$NEOVIM_SHA256_CHECKSUM" "$SHA256_CHECKSUMS"; then
+    local EXEC_FILES_DIR="$HOME/.local/bin"
+
     tar xzf "$NEOVIM_TARBALL" -C "$HOME"
 
-    ln -sf "$NEOVIM_DIR/bin/nvim" "$HOME/.local/bin/"
+    mkdir -p "$EXEC_FILES_DIR"
+    ln -sf "$NEOVIM_DIR/bin/nvim" "$EXEC_FILES_DIR"
   fi
 
   rm -rf "$NEOVIM_TARBALL" "$SHA256_CHECKSUMS"
