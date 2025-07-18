4038aa34518f688fc70ec180c4d7708ad4370daf
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-07-18 11:46:26 -0300

    fix(apt-packages): remove network-manager-gnome from PACKAGES array
    - Improve network-manager's comment

--- core-functions/apt-packages.sh
+++ core-functions/apt-packages.sh
@@ -2,43 +2,42 @@ install_apt_packages() {
   display_header "Installing APT packages"
 
   local PACKAGES=(
-    "arandr"                # screen layout
-    "bat"                   # replacement for cat
-    "chrony"                # synchronize clock
-    "curl"                  # kitty
-    "exa"                   # replacement for ls
-    "fcitx5"                # japanese input
-    "fcitx5-mozc"           # japanese input
-    "fd-find"               # fzf
-    "feh"                   # wallpapers
-    "flatpak"               # newer packages
-    "git"                   # programming / dotfiles / tmux-plugins
-    "libavcodec-extra"      # media codecs
-    "libnotify-bin"         # desktop notifications
-    "locales"               # locale-gen
-    "lxappearance"          # gtk theme
-    "maim"                  # maimpick
-    "mpv"                   # default video player
-    "network-manager"       # nm-applet
-    "network-manager-gnome" # nm-applet
-    "nsxiv"                 # default image viewer
-    "pipewire-audio"        # pipewire
-    "qt5ct"                 # qt5 theme
-    "qt6ct"                 # qt6 theme
-    "ripgrep"               # telescope.nvim
-    "rofi"                  # app launcher / maimpick
-    "stow"                  # dotfiles
-    "tmux"                  # programming
-    "unzip"                 # extract .zip files
-    "vlc"                   # media dependencies
-    "webp"                  # maimpick
-    "wget"                  # maimpick / neovim
-    "xclip"                 # maimpick
-    "xdotool"               # maimpick
-    "xinit"                 # startx
-    "zathura"               # default document viewer
-    "zip"                   # create .zip files
-    "zsh"                   # replacement for bash
+    "arandr"           # screen layout
+    "bat"              # replacement for cat
+    "chrony"           # synchronize clock
+    "curl"             # kitty
+    "exa"              # replacement for ls
+    "fcitx5"           # japanese input
+    "fcitx5-mozc"      # japanese input
+    "fd-find"          # fzf
+    "feh"              # wallpapers
+    "flatpak"          # newer packages
+    "git"              # programming / dotfiles / tmux-plugins
+    "libavcodec-extra" # media codecs
+    "libnotify-bin"    # desktop notifications
+    "locales"          # locale-gen
+    "lxappearance"     # gtk theme
+    "maim"             # maimpick
+    "mpv"              # default video player
+    "network-manager"  # network connections
+    "nsxiv"            # default image viewer
+    "pipewire-audio"   # pipewire
+    "qt5ct"            # qt5 theme
+    "qt6ct"            # qt6 theme
+    "ripgrep"          # telescope.nvim
+    "rofi"             # app launcher / maimpick
+    "stow"             # dotfiles
+    "tmux"             # programming
+    "unzip"            # extract .zip files
+    "vlc"              # media dependencies
+    "webp"             # maimpick
+    "wget"             # maimpick / neovim
+    "xclip"            # maimpick
+    "xdotool"          # maimpick
+    "xinit"            # startx
+    "zathura"          # default document viewer
+    "zip"              # create .zip files
+    "zsh"              # replacement for bash
   )
   local EXEC_FILES_DIR="$HOME/.local/bin"
   local SERVICES=(
