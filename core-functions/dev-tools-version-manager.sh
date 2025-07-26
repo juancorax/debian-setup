4e20a2af1e43cc03aa200070dee8e08dc636212a
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2025-07-26 15:33:44 -0300

    feat(dev-tools): replace PHP dependencies with Lua ones

--- core-functions/dev-tools-version-manager.sh
+++ core-functions/dev-tools-version-manager.sh
@@ -7,35 +7,13 @@ install_dev_tools_version_manager() {
 install_dev_tools() {
   display_header "Installing dev tools"
 
-  local PHP_DEPENDENCIES=(
-    "autoconf"
-    "bison"
+  local LUA_DEPENDENCIES=(
     "build-essential"
-    "curl"
-    "default-libmysqlclient-dev"
-    "gettext"
-    "git"
-    "libcurl4-openssl-dev"
-    "libedit-dev"
-    "libgd-dev"
-    "libicu-dev"
-    "libjpeg-dev"
-    "libonig-dev"
-    "libpng-dev"
-    "libpq-dev"
-    "libreadline-dev"
-    "libsqlite3-dev"
-    "libssl-dev"
-    "libxml2-dev"
-    "libzip-dev"
-    "openssl"
-    "pkg-config"
-    "re2c"
-    "zlib1g-dev"
+    "linux-headers-$(uname -r)"
   )
 
   sudo apt update
-  sudo apt install -y "${PHP_DEPENDENCIES[@]}"
+  sudo apt install -y "${LUA_DEPENDENCIES[@]}"
 
   eval "$(~/.local/bin/mise activate bash)"
   mise install
